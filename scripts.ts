c6fe5e0127f92e1bcdeaf63f21f54c06f072b366
Jacob Hummer <jcbhmr@outlook.com>
2024-02-02 04:53:05 +0000

    save point

--- scripts.ts
+++ scripts.ts
@@ -10,12 +10,14 @@ import {
   copyFile,
   chmod,
   mkdir,
+  cp,
 } from "node:fs/promises";
 import { resolve, join, dirname, basename } from "node:path";
 import JSONParse from "npm:json-parse-even-better-errors";
 import assert from "node:assert/strict";
 import { $ } from "npm:execa";
 import { temporaryDirectory } from "npm:tempy";
+import { renderFile } from "npm:ejs";
 
 const langs = ["zh", "es"];
 
@@ -34,6 +36,8 @@ async function build() {
     console.debug(`Will create cargo script at ${cargoScriptFile}`);
     const outPagesFile = resolve("out", `pages-${lang}.json`);
     console.debug(`Will create pages data at ${outPagesFile}`);
+    const langOutDir = resolve("_site", lang);
+    console.debug(`Will write output to ${langOutDir}`);
 
     // prettier-ignore
     // deno-fmt-ignore
@@ -48,20 +52,17 @@ typst-docs = { path = ${JSON.stringify(join(langRoot, "crates", "typst-docs"))} 
 typst = { path = ${JSON.stringify(join(langRoot, "crates", "typst"))} }
 typst-render = { path = ${JSON.stringify(join(langRoot, "crates", "typst-render"))} }
 serde_json = "1.0.113"
-dataurl = "0.1.2"
-mime_guess = "2.0.4"
 html-escape = "0.2.13"
 \`\`\`
 
 use typst_docs::{Resolver, Html, Commit, provide};
 use typst::layout::Frame;
-use std::env::args;
-use std::fs::File;
+use std::env::var;
+use std::fs::{File, create_dir_all};
 use std::io::Write;
-use dataurl::DataUrl;
-use mime_guess;
 use typst_render::render;
 use typst::visualize::Color;
+use std::path::Path;
 
 struct MyResolver;
 impl Resolver for MyResolver {
@@ -72,23 +73,20 @@ impl Resolver for MyResolver {
   fn image(&self, filename: &str, data: &[u8]) -> String {
     eprintln!("image(self, {:?}, [{} bytes])", filename, data.len());
 
-    let mut data_url = DataUrl::new();
-    data_url.set_data(data);
-
-    let mime_type = mime_guess::from_path(filename).first_or_octet_stream();
-    data_url.set_media_type(Some(mime_type.as_ref().into()));
-
-    data_url.to_string()
+    format!("/assets/docs/{}", filename)
   }
 
   fn example(&self, hash: u128, source: Option<Html>, frames: &[Frame]) -> Html {
     eprintln!("example(self, {}, {:?}, [{} frames])", hash, source.is_some(), frames.len());
 
     let pixmap = render(frames.first().unwrap(), 2.0, Color::WHITE);
+    let out_dir = var("OUT_DIR").expect("OUT_DIR not set");
     if let Some(source) = source {
-      let mut data_url = DataUrl::new();
-      data_url.set_data(pixmap.data());
-      data_url.set_media_type(Some("image/png".into()));
+      let filename = format!("example-{}.png", hash);
+      let path = Path::new(&out_dir).join("assets/docs").join(&filename);
+      create_dir_all(path.parent().unwrap()).unwrap();
+      pixmap.save_png(path).unwrap();
+      let src_url = format!("/assets/docs/{}", filename);
 
       Html::new(format!(
         r#"<div class="previewed-code">
@@ -97,7 +95,7 @@ impl Resolver for MyResolver {
             <img src="{}" alt="Preview" width="480" height="190" />
           </div>
         </div>"#,
-        html_escape::encode_text(source.as_str()), data_url.to_string()
+        html_escape::encode_text(source.as_str()), src_url
       ))
     } else {
       Html::new(String::new())
@@ -112,8 +110,10 @@ impl Resolver for MyResolver {
 }
 
 fn main() {
-  let out_pages_file = args().nth(1).unwrap();
+  let out_pages_file = var("OUT_PAGES_FILE").expect("OUT_PAGES_FILE not set");
+  let out_dir = var("OUT_DIR").expect("OUT_DIR not set");
   println!("Writing pages data to {}", out_pages_file);
+  println!("Writing out files to {}", out_dir);
 
   println!("Running 'provide()' with MyResolver to generate pages data");
   let pages = provide(&MyResolver);
@@ -132,10 +132,51 @@ fn main() {
     console.debug(`Created ${cargoScriptFile}`);
 
     console.debug(`Running cargo script to generate ${outPagesFile}`);
-    await $({ stdio: "inherit" })`${cargoScriptFile} ${outPagesFile}`;
+    await $({
+      stdio: "inherit",
+      env: {
+        OUT_PAGES_FILE: resolve(outPagesFile),
+        OUT_DIR: langOutDir,
+      },
+    })`${cargoScriptFile}`;
 
-    const pagesData = JSONParse(await readFile(outPagesFile, "utf8"));
+    await mkdir(join(langOutDir, "assets", "docs"), { recursive: true });
+    await cp(
+      join(langRoot, "assets", "files"),
+      join(langOutDir, "assets", "docs"),
+      { recursive: true }
+    );
+    console.debug(
+      `Copied ${lang} assets to ${join(langOutDir, "assets", "docs")}`
+    );
+
+    const pages = JSONParse(await readFile(outPagesFile, "utf8"));
     console.debug(`Got pages data for ${lang}`);
+
+    const pagesFlat = pages.flatMap(function f(page: any) {
+      return [page, ...page.children.flatMap(f)];
+    });
+    console.info(`There are ${pagesFlat.length} pages in ${lang}`);
+
+    for (const page of pagesFlat) {
+      const htmlFile = join(
+        langOutDir,
+        page.route,
+        page.route.endsWith("/") ? "index.html" : ""
+      );
+
+      const layoutFile = resolve("_layouts", `${page.body.kind}.ejs`);
+      console.debug(`Rendering ${htmlFile} using ${layoutFile}`);
+      const renderedHTML = await renderFile(layoutFile, page, { async: true });
+
+      const newAssets = `${process.env.BASE_PATH || "/"}${lang}/assets/`;
+      const tweakedHTML = renderedHTML.replaceAll("/assets/", newAssets);
+      console.debug(`Tweaked to '${newAssets}'`);
+
+      await mkdir(dirname(htmlFile), { recursive: true });
+      await writeFile(htmlFile, tweakedHTML);
+      console.debug(`Wrote ${htmlFile}`);
+    }
 
     console.groupEnd();
   }
